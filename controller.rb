7747add890d035965757ada77441123037b7d357
Becky <rebeccajoynielsen@gmail.come>
2019-03-19 22:27:50 +0000

    create ruby model for game logic

--- controller.rb
+++ controller.rb
@@ -5,31 +5,19 @@ require_relative('./models/rps_game')
 also_reload('./models/*')
 
 get '/rock/:play' do
-  if params[:play] == "scissor"
-     "rock beats scissor!"
-  elsif params[:play] == "paper"
-    "paper beats rock!"
-  else
-    "uh-oh, try again!"
-  end
+  opp_move = params[:play]
+  game = Game.new()
+  game.play_against_rock(opp_move)
 end
 
 get '/scissor/:play' do
-  if params[:play] == "paper"
-     "scissor beats paper!"
-  elsif params[:play] == "rock"
-    "rock beats scissor!"
-  else
-    "uh-oh, try again!"
-  end
+ opp_move = params[:play]
+ game = Game.new()
+ game.play_against_scissor(opp_move)
 end
 
 get '/paper/:play' do
-  if params[:play] == "rock"
-     "paper beats rock!"
-  elsif params[:play] == "scissor"
-    "scissor beats paper!"
-  else
-    "uh-oh, try again!"
-  end
+  opp_move = params[:play]
+  game = Game.new()
+  game.play_against_paper(opp_move)
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,58 +1,35 @@
 class Game
 
-  def initialize(play1, play2)
-    @play1 = play1
-    @play2 = play2
+  def initialize
   end
 
-  # def play_game(play1, play2)
-  #
-  #   play1_wins = "#{@play1} wins!"
-  #   play2_wins = "#{@play2} wins!"
-  #   two_same = "oooh, try again!"
-  #
-  #
-  #   if @play1 == @play2
-  #     return two_same
-  #   elsif @play1 == "rock" && @play2 == "scissor"
-  #     || @play1 == "scissor" && @play2 == "paper"
-  #     || @play1 == "paper" && @play2 == "rock"
-  #     return play1_wins
-  #   elsif @play1 == "rock" && @play2 == "paper"
-  #     || @play1 == "scissor" && @play2 == "rock"
-  #     || @play1 == "paper" && @play2 == "scissor"
-  #     return play2_wins
-  #   else return "uh-oh"
-  #   end
-  #
-  # end
-  #
-  # #def play_game
-  #   # if @play1 == "rock"
-  #   #   if @play2 == "paper"
-  #   #     return "#{@play2} wins!"
-  #   #   elsif @play2 == "scissor"
-  #   #     return "#{@play1} wins!"
-  #   #   else
-  #   #     return "ooooh, try again!"
-  #   #   end
-  #   # elsif @play1 == "scissor"
-  #   #     if @play2 == "rock"
-  #   #       return "#{@play2} wins!"
-  #   #     elsif @play2 == "paper"
-  #   #       return "#{@play1} wins!"
-  #   #     else
-  #   #       return "ooooh, try again!"
-  #   #     end
-  #   # elsif @play1 == "paper"
-  #   #   if @play2 == "scissor"
-  #   #     return "#{@play2} wins!"
-  #   #   elsif @play2 == "rock"
-    #     return "#{@play1} wins!"
-    #   else
-    #     return "ooooh, try again!"
-    #   end
-    # end
-  #end
+  def play_against_rock(opp_move)
+    if opp_move == "scissor"
+       "rock beats scissor!"
+    elsif opp_move == "paper"
+      "paper beats rock!"
+    else
+      "uh-oh, try again!"
+    end
+  end
 
+  def play_against_scissor(opp_move)
+    if opp_move == "paper"
+       "scissor beats paper!"
+    elsif opp_move == "rock"
+      "rock beats scissor!"
+    else
+      "uh-oh, try again!"
+    end
+  end
+
+  def play_against_paper(opp_move)
+    if opp_move == "rock"
+       "paper beats rock!"
+    elsif opp_move == "scissor"
+      "scissor beats paper!"
+    else
+      "uh-oh, try again!"
+    end
+  end
 end
